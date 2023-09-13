b51dd00ecd8c613979932c0855a898e512d2d935
Logesh <logeshwaran.mani@itp.objectfrontier.com>
2023-09-13 10:12:56 +0530

    Repo5

---
+++ Sample5.cs
@@ -0,0 +1,16 @@
+﻿public class Sample5{
+
+    public static void Main(string[] args){
+
+       string StudentName;
+       double TotalMarks;
+
+        // Find the class based on Total marks : >=60 - First, 50 - 59 - Second, else Fail
+        // The student has scored First/Second/Fail class
+        // Complete the program and push into Github and Bitbucket
+
+        
+          Console.WriteLine($"The {StudentName} has scored ..... ");
+       
+    }
+}
