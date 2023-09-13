e4e52776eb67bbcfea42802993ec2a0fede11892
Logesh <logeshwaran.mani@itp.objectfrontier.com>
2023-09-13 09:10:32 +0530

    Sample1 Commit

---
+++ Sample1.cs
@@ -0,0 +1,18 @@
+﻿class Sample1
+{
+public static void Main(string[] args){
+    
+    int num1=3, num2=5, sum=0;
+    
+    // Find the sum of squares of the given two numbers
+    // Write your name and empid in the output
+    // Complete the program and push into Github and Bitbucket
+
+    
+    //sum = 
+    //Console.WriteLine("Sum = ");
+
+}
+
+
+}
