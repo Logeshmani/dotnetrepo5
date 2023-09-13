87baedc77c75c386df50ede6bd980ed89e594556
Logesh <logeshwaran.mani@itp.objectfrontier.com>
2023-09-13 10:00:13 +0530

    Repo3

---
+++ Sample3.cs
@@ -0,0 +1,18 @@
+﻿public class Sample3{
+
+    public static void Main(string[] args){
+
+        string PersonName;
+        int age;
+
+        // Find the person is eligible for getting Driving License or not (18 or above)
+        // Complete the program and push into Github and Bitbucket
+
+        /*
+          if(age )
+          Console.WriteLine("");
+          else
+          Console.WriteLine("");
+        */
+    }
+}
